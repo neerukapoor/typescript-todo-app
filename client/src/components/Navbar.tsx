f39e3021391305eeefb283f6b8286026c9d7ce6f
Neeru Kapoor <467neeru@gmail.com>
2023-10-29 22:33:40 +0530

    Navbar

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -8,24 +8,45 @@ import {useEffect, useState} from 'react';
 
 function Navbar() {
 
-    const [username, setUserName] = useState(null);
+    const [username, setUserName] = useState('');
 
     useEffect(() => {
         fetch("http://localhost:3000/me", {
             method:"GET",
             headers: {
-                "token": "Bearer" + localStorage.getItem("jwtToken")
+                "token": "Bearer " + localStorage.getItem("jwtToken")    
             }
         }).then((res)=>{
             return res.json()
         }).then((data) => {
+            setUserName(data.username)
             console.log(data);
         })
     }, [])
 
-    if(username) {
+    if(username !== "") {
         return <>
-            lsdjf
+            <Box sx={{ flexGrow: 1 }}>
+                <AppBar position="static">
+                    <Toolbar>
+                    <IconButton
+                        size="large"
+                        edge="start"
+                        color="inherit"
+                        aria-label="menu"
+                        sx={{ mr: 2 }}
+                    >
+                    </IconButton>
+                    <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                        <a href="/" style={{textDecoration:"none", color:"white"}}>TODO</a>
+                    </Typography>
+                    <Button color="inherit" href='/login' onClick={() => {localStorage.setItem("jwtToken", '')} }>Logout</Button>
+                    <div>
+                        {/* {username} */}
+                    </div>
+                    </Toolbar>
+                </AppBar>
+            </Box>
         </>
     }
      

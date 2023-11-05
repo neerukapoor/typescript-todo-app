e1468ca94c8bf277aec839bec4191435dbf7fb73
Neeru Kapoor <467neeru@gmail.com>
2023-11-05 19:27:46 +0530

    a small change in dp

--- server/db/auth.ts
+++ server/db/auth.ts
@@ -2,7 +2,11 @@ import mongoose from 'mongoose';
 
 const UserSchema = new mongoose.Schema({
     username: {type:String},
-    password: {type:String}
+    password: {type:String},
+    todo: [{
+        todoTitle: {type:String},
+        todoDescription: {type:String}
+    }]
 })
 
 export const User = mongoose.model('User', UserSchema);

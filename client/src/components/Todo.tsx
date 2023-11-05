b49761bb35922401cae754f4be6719ff160dc646
Neeru Kapoor <467neeru@gmail.com>
2023-11-05 19:18:54 +0530

    Starting todo frontend

--- client/src/components/Todo.tsx
+++ client/src/components/Todo.tsx
@@ -1,6 +1,41 @@
+import Card from '@mui/material/Card';
+import {useState, useEffect} from 'react';
+
+
+type TodoItem = {
+    _id: string,
+    todoTitle: string,
+    todoDescription: string
+}
+
 function Todo() {
+
+    const [todos, setTodos] = useState<TodoItem[]>([]);
+
+    useEffect(() => {
+        fetch("http://localhost:3000/", {
+            method:"GET",
+            headers: {
+                "Content-type": "application/json",
+                "jwtToken": "Bearer " + localStorage.getItem("jwtToken")    
+            }
+        }).then((res)=>{
+            return res.json()
+        }).then((data) => {
+            setTodos(data.todos)
+            console.log(data)
+            console.log(data.todos[0]._id);
+        })
+    },[])
+
     return <> 
-        asldhf
+        <div style={{display:"flex", justifyContent:"center", alignItems:"center", height:"85vh"}}>
+            <Card sx={{ minWidth: 275, padding:"5px" }}>
+                {todos.map((todo) => (
+                    <li key={todo._id}>{todo.todoTitle}</li>
+                ))}
+            </Card>
+        </div>
     </>
 }
  
